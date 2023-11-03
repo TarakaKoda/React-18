fe6842fb99c587fc45a4fb6f4a4265641c10701b
TarakaKoda <taraka.koda@gmail.com>
2023-11-03 15:20:34 +0530

    Create ListGroup component with dynamic item rendering and conditional message.

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,20 @@
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+//   const items = [] //! no items in the array
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
