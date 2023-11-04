e3cfd71c3eb19d8bf86e2dd36751ff1472f98fb8
TarakaKoda <taraka.koda@gmail.com>
2023-11-04 13:36:11 +0530

    Build the button and alter components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
-  return <div><ListGroup/></div>
+  let [isAlertVisible, setAlertVisibility] = useState(false);
+  const handleClose = () => setAlertVisibility(false)
+  const handleClick = () => setAlertVisibility(true);
+
+  return (
+    <div>
+      {isAlertVisible && <Alert onClose={handleClose}>Button Clicked</Alert>}
+      <Button onClick={handleClick}>Submit</Button>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,36 @@
-function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-//   const items = [] //! no items in the array
+import { useState } from "react";
+
+//* PASsing Data via Props using the interface (TypeScript)
+//* Interface is a Type
+interface Props {
+  items: string[];
+  heading: string;
+  OnSelectItem: (item: string) => void;
+}
+
+function ListGroup({items, heading, OnSelectItem}: Props) {
+  //* Hook: A Hook is a function that allow us to tap-in to the built-in features in react.
+  //* useState(): Using the state hook we can tell to the react the the value or the state of the variable will change in the future.
+  //* When we change the state of a component the react will monitor the change and update the DOM.
+  let [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              OnSelectItem(item);
+              }}>
             {item}
           </li>
         ))}
