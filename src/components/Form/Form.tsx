b0e4264ed53c7b04b6dd5b85b61a61a6db8a2ec5
TarakaKoda <taraka.koda@gmail.com>
2023-11-07 01:26:53 +0530

    Applying validations.

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,12 +1,21 @@
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const {register, handleSubmit} = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
-  }
- 
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
@@ -14,22 +23,37 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register('name')}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="mt-1 text-danger">The name field is empty</p>
+        )}
+
+        {errors.name?.type === "minLength" && (
+          <p className="mt-1 text-danger">
+            The name must be at least 3 characters.
+          </p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           age
         </label>
         <input
-          {...register('age')}
+          {...register("age", { required: true, min: 18 })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age?.type === "required" && (
+          <p className="mt-1 text-danger">The age field is empty</p>
+        )}
+        {errors.age?.type === "min" && (
+          <p className="text-danger">Age must be minimum of 18</p>
+        )}
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
