6a1c2b916747ba3531b964c5a6b002a804d22c01
TarakaKoda <taraka.koda@gmail.com>
2023-11-07 18:24:25 +0530

    Build the Expenses Tracker project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./Expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./Expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./Expense-tracker/components/ExpenseForm";
 import "./App.css";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Movies", amount: 2000, category: "Entertainment" },
+  ]);
+
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleDelete = (id: number) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
+  const handleSelect = (category: string) => {
+    setSelectedCategory(category);
+  };
+
   return (
-    <Form/>
+    <>
+      <div className="mb-5">
+        <ExpenseForm onSubmit={(expense) => setExpenses([...expenses, {...expense, id: expenses.length + 1}])} />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={handleSelect} />
+      </div>
+      <ExpenseList expenses={visibleExpenses} onDelete={handleDelete} />
+    </>
   );
 }
 
