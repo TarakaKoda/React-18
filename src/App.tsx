6b6799cf78908c1770952d89c52daec098584172
TarakaKoda <taraka.koda@gmail.com>
2023-11-08 15:08:11 +0530

    Understanding effect hook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
-import { useState } from "react";
-import ExpenseList from "./Expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./Expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./Expense-tracker/components/ExpenseForm";
+
+import { useEffect, useRef } from "react";
 import "./App.css";
 
+// * We know that the react expects us to build the components purely, which means the output must be always same as we pass the same input.
+// this is called as a pure function.
+// but while calling the backend and changing the DOM elements we don't return any render elements, so we need to move that code in different function called useEffect()
+//* The useEffect() will be executed after the rendering of the component. we can use this function to make a call to the backend. 
+
+
 function App() {
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "Movies", amount: 2000, category: "Entertainment" },
-  ]);
 
-  const [selectedCategory, setSelectedCategory] = useState("");
-
-  const handleDelete = (id: number) => {
-    setExpenses(expenses.filter((expense) => expense.id !== id));
-  };
-
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
-
-  const handleSelect = (category: string) => {
-    setSelectedCategory(category);
-  };
+  const ref = useRef<HTMLInputElement>(null);
+  // This effect hook is executed after the render of the component.
+  // We can use this hook to manipulate the DOM elements or we can call any server for the backend data. 
+  // Same as the state and ref hook we can create effect hook right after one and another.
+  // Similarly we to call this effect hook at the top most level of the component.
+  // We cannot use this in the any of the conditional statement or looping statements.
+  useEffect(() => {
+    //* Side effect: It is changing the outside component, Technically Modifying the DOM.
+    if(ref.current) ref.current.focus(); // This will be executed after the completions of rendering of this component.
+  }); 
+  
+  useEffect(() => {
+    document.title = 'My App' // The document title will be changed after rendering of this component.
+  })
 
   return (
-    <>
-      <div className="mb-5">
-        <ExpenseForm onSubmit={(expense) => setExpenses([...expenses, {...expense, id: expenses.length + 1}])} />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter onSelectCategory={handleSelect} />
-      </div>
-      <ExpenseList expenses={visibleExpenses} onDelete={handleDelete} />
-    </>
+    <div>
+      <input ref={ref} type="text" className="form-control" />
+    </div>
   );
 }
 
