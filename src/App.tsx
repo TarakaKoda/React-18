73a3dee9315ad7934ab65f3a1d377af0f21f58d8
TarakaKoda <taraka.koda@gmail.com>
2023-11-09 13:36:20 +0530

    Practicing forms.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
-
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
-// * We know that the react expects us to build the components purely, which means the output must be always same as we pass the same input.
-// this is called as a pure function.
-// but while calling the backend and changing the DOM elements we don't return any render elements, so we need to move that code in different function called useEffect()
-//* The useEffect() will be executed after the rendering of the component. we can use this function to make a call to the backend. 
-
-
 function App() {
-
+  const [category, setCategory] = useState<string[]>([]);
+  const [message, setMessage] = useState("");
   const ref = useRef<HTMLInputElement>(null);
-  // This effect hook is executed after the render of the component.
-  // We can use this hook to manipulate the DOM elements or we can call any server for the backend data. 
-  // Same as the state and ref hook we can create effect hook right after one and another.
-  // Similarly we to call this effect hook at the top most level of the component.
-  // We cannot use this in the any of the conditional statement or looping statements.
+
   useEffect(() => {
-    //* Side effect: It is changing the outside component, Technically Modifying the DOM.
-    if(ref.current) ref.current.focus(); // This will be executed after the completions of rendering of this component.
-  }); 
-  
+    if (ref.current) ref.current.focus();
+  });
+
   useEffect(() => {
-    document.title = 'My App' // The document title will be changed after rendering of this component.
-  })
+    document.title = "My App";
+  });
+
+  const handleClick = () => {
+    if (ref.current) {
+      const newCategory = ref.current.value;
+      if (category.includes(newCategory)) {
+        setMessage(`${newCategory} already exist`);
+      } else {
+        if (ref.current.value) {
+          setCategory([...category, ref.current.value]);
+          setMessage(`New Category has been added ${newCategory}`);
+        }
+      }
+    }
+  };
 
   return (
-    <div>
-      <input ref={ref} type="text" className="form-control" />
-    </div>
+    <>
+      <div className="mb-3">
+        <input ref={ref} type="text" className="form-control mb-3" />
+        {message && (
+          <p
+            className={
+              message.includes("already") ? "text-danger" : "text-success"
+            }>
+            {message}
+          </p>
+        )}
+        <button type="submit" className="btn btn-primary" onClick={handleClick}>
+          Submit
+        </button>
+      </div>
+      <div className="mb-3">
+        <select className="form-select mb-3">
+          <option value="">Select category</option>
+          {category &&
+            category.map((element, index) => (
+              <option key={index} value={element}>
+                {element}
+              </option>
+            ))}
+        </select>
+      </div>
+    </>
   );
 }
 
