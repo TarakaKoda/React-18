426867ffa8200f9a690120567b025256cc6a5cc3
TarakaKoda <taraka.koda@gmail.com>
2023-11-22 15:51:07 +0530

    Performed CURD operations using fetch method.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import GettingUsersDataUsingFetch from "./practice/ConnectingBackend/FetchMethod/01-BasicFetch";
 import FetchingDataUsingFetchAsyncAndAwait from "./practice/ConnectingBackend/FetchMethod/02-AsyncAndAwait";
 
-
-
 const App = () => {
   return (
-    <FetchingDataUsingFetchAsyncAndAwait/>
-  )
-}
+    <>
+      <GettingUsersDataUsingFetch />
+      <FetchingDataUsingFetchAsyncAndAwait />
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/practice/ConnectingBackend/FetchMethod/01-BasicFetch.tsx
+++ src/practice/ConnectingBackend/FetchMethod/01-BasicFetch.tsx
@@ -5,12 +5,15 @@ interface User {
   name: string;
 }
 // * simplest fetch you can use and still have error handling.
-const url = 'https://jsonplaceholder.typicode.com/users'
+const url = "https://jsonplaceholder.typicode.com/users";
 
 const GettingUsersDataUsingFetch = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [errors, setErrors] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const controller = new AbortController();
+    setIsLoading(true);
     fetch(url, {
       signal: controller.signal,
     })
@@ -21,20 +24,138 @@ const GettingUsersDataUsingFetch = () => {
       })
       .then((userData) => {
         setUsers(userData);
+        setIsLoading(false);
       })
-      .catch((error) => console.warn(error));
+      .catch((error) => {
+        if (error instanceof DOMException && error.name === "AbortError")
+          return;
+        setErrors(error.message);
+        setIsLoading(false);
+      });
 
     return () => controller.abort();
   }, []);
 
+  const handleDelete = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    const request = new Request(
+      `https://jsonplaceholder.typicode.com/users/${user.id}`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    fetch(request)
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(
+            `Unable to delete the user. Status: ${response.status}`
+          );
+      })
+      .catch((error) => {
+        if (error instanceof TypeError) {
+          setErrors("Network error. Unable to reach the server.");
+          setUsers(originalUsers);
+        } else setErrors(error.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const handleAddUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "taraka" };
+
+    setUsers([newUser, ...users]);
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(`Unable to add user! Status: ${response.status}`);
+        return response.json();
+      })
+      .then((newUserData) => {
+        setUsers([newUserData, ...users]);
+      })
+      .catch((error) => {
+        if (error instanceof TypeError)
+          setErrors("Network error. Unable to reach the server.");
+        else {
+          setErrors(error.message);
+          setUsers(originalUsers);
+        }
+      });
+  };
+
+  const handleUpdate = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { id: user.id, name: user.name + "!" };
+
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    fetch(`${url}/${user.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedUser),
+    })
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(
+            `Unable to Update ${user.name} to ${updatedUser.name}. Status: ${response.status}`
+          );
+        return response.json();
+      })
+      .then((updatedUserData) => {
+        setUsers(users.map((u) => (u.id === user.id ? updatedUserData : u)));
+      })
+      .catch((error) => {
+        if (error instanceof TypeError) {
+          setErrors("Network error. Unable to reach the server.");
+          setUsers(originalUsers);
+        } else {
+          setErrors(error.message);
+          setUsers(originalUsers);
+        }
+      });
+  };
+
   return (
-    <ul className="list-group">
-      {users.map((user) => (
-        <li className="list-group-item" key={user.id}>
-          {user.name}
-        </li>
-      ))}
-    </ul>
+    <>
+      {isLoading && <div className="spinner-border"></div>}
+      {errors && <p className="text-danger">{errors}</p>}
+      <button className="btn btn-primary mb-3" onClick={handleAddUser}>
+        Add
+      </button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            className="list-group-item d-flex justify-content-between"
+            key={user.id}>
+            {user.name}
+            <div className="button-wrapper d-flex gap-2">
+              <button
+                className="btn btn-outline-secondary"
+                onClick={() => handleUpdate(user)}>
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => handleDelete(user)}>
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
